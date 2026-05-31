69fd705232c50d94a9cf331b9b0e1224c4e3bfb9
Natálie Sedláková <natalie.sedlakova@betrian.cz>
2026-05-31 15:38:31 +0200

    feat: audio files

--- dist-electron/preload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('djApp', {
-    platform: process.platform,
-    versions: process.versions,
-});
-//# sourceMappingURL=preload.js.map
